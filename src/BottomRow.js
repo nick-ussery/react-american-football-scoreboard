013240f4146ac54fafd61ed48257523c80a20b60
nick-ussery <nick.ussery@gmail.com>
2020-04-08 17:34:06 -0500

    finished main

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,10 @@
-import React from "react";
 import "./App.css";
+import {useState} from 'react';
+import React, { Component } from 'react';
 
 const BottomRow = () => {
+  const [quarter, setQuarter] = useState(1);
+
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +21,9 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+  <div className="quarter__value" onClick={()=>{
+      if (quarter<4){setQuarter(quarter+1)}
+  }}>{quarter}</div>
       </div>
     </div>
   );
